d95f49031fa2f8af5244c19bc158a9451a96d565
Ravinder2301 <132249859+Ravinder2301@users.noreply.github.com>
2024-11-07 15:13:34 +0530

    Delete Frontend directory

--- Frontend/src/typings/he.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'he' {
-    export function decode(html: string): string;
-    export function encode(text: string): string;
-  }
-  
--- Frontend/src/typings/jquery.d.ts
+++
@@ -1,8 +0,0 @@
-// typings/jquery.d.ts
-
-declare module 'jquery' {
-    interface JQuery<TElement = HTMLElement> {
-      modal(action: string): this;
-    }
-  }
-  
